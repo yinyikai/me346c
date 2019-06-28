b5e0a38641cb6aac714e9a39c771b4dd3ff51c94
yinyikai <46612096+yinyikai@users.noreply.github.com>
2019-06-27 17:44:36 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ echo Running ME346C jupyter notebook setup ...
 jupyter notebook --generate-config
 cp -f ./jupyter_notebook_config.py ~/.jupyter/
 echo Please set a password for your Jupyter notebook
-python -m notebook.auth password
+python3 -m notebook.auth password
 echo Setup finished.
